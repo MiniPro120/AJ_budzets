826571accbe692fb40800d7a11072c226d651369
Artis Jencītis <aj2565@gymn.local>
2024-02-21 10:33:19 +0200

    Pievienoti mainīgie, funkcija

--- AJ_budzets/Form1.cs
+++ AJ_budzets/Form1.cs
@@ -21,5 +21,30 @@ namespace AJ_budzets
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int alga = Convert.ToInt32(Math.Round(nr_alga.Value, 0));
+            int papildus = Convert.ToInt32(Math.Round(nr_papildus.Value, 0));
+            int ire = Convert.ToInt32(Math.Round(nr_ire.Value, 0));
+            int partika = Convert.ToInt32(Math.Round(nr_partika.Value, 0));
+            int komunalie = Convert.ToInt32(Math.Round(nr_komunalie.Value, 0));
+
+            budzeta_aprekins(alga, papildus, ire, partika, komunalie);
+
+        }
+
+        private void budzeta_aprekins(int alga, int papildus, int ire, int partika, int komunalie)
+        {
+            int ienakumi = alga + papildus;
+
+            int izdevumi = ire + partika + komunalie;
+
+            int budzets = ienakumi - izdevumi;
+
+            txt_budzets.Text = budzets.ToString();
+
+
+        }
     }
 }
